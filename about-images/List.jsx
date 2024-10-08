f259dbd0e86cc1e280f4e55376df8bfd860c4cfd
SIDHANTBARAPATRE <122191343+SIDHANTBARAPATRE@users.noreply.github.com>
2024-10-08 18:20:14 +0530

    Add files via upload
    blog

---
+++ about-images/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Gupchup</li>
+      <li>Mango</li>
+      <li>Pineapple</li>
+    </ul>
+  );
+}
+
+export default List;
